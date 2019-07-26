c8eeec7a6b2c7e1f1d3b01eb8e6baac19cd722c8
肖勖 <xiao@xiaoxudeMacBook-Pro.local>
2019-07-26 21:35:49 +0800

    controller and .yml

--- src/main/java/com/xiao/controller/HelloController.java
+++ src/main/java/com/xiao/controller/HelloController.java
@@ -1,15 +1,13 @@
 package com.xiao.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World!";
     }
 }
